ca056ff21e1fbdecf77f6c9c696e00424798a855
Marigameo <mariappangameo@gmail.com>
2020-11-12 14:26:52 +0530

    replaceState instead of location

--- pages/orderStatus.js
+++ pages/orderStatus.js
@@ -1,12 +1,14 @@
 import styles from '../styles/Home.module.css'
 import React, { useEffect } from 'react';
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 export default function orderStatus () {
+    const router = useRouter()
 
     useEffect(() => {
         window.addEventListener('popstate', function (event) {
-            window.location.replace('/');
+            window.history.replaceState(null, null, '/');
         }, false);
     })
 
